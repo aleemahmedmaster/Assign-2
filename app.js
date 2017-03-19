f7d5014f6c2caef2fc74a5a7948042de65e8313e
Syed Aleem Ahmed <aleemahmedmaster@gmail.com>
2017-03-20 00:05:19 +0500

    Assign-2

---
+++ app.js
@@ -0,0 +1,21 @@
+var a = +prompt ("Enter Your Percentage");
+if (a > 80)
+{
+    alert ("Congrats you've got Grade A+");
+}
+else if (a>=70)
+{
+    alert  ("Congrats you've got Grade A");
+}
+else if (a>=60)
+{
+    alert  ("Congrats you've got Grade B");
+}
+else if (a>=50)
+{
+    alert  ("You've got Grade C");
+}
+else if (a<50)
+{
+    alert  ("Sorry you've Failed");
+}
